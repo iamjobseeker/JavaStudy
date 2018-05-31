646bcc714aa5487e3e55fa1b4b41d084aea687f1
iamjobseeker <joeydays@naver.com>
2018-05-31 15:26:03 +0900

    5/31 스터디

---
+++ MyOwn/src/review/ClassReview.java
@@ -0,0 +1,18 @@
+package review;
+
+public class ClassReview {
+	
+	public static void printnum(int num) {
+		for(int i=0; i<num; i++) {
+		System.out.print((i+1)+"positive ");
+		}
+	}
+	
+	public static void printstr(int num, String str) {
+		for(int i=0; i<num; i++) {
+			System.out.print((i+1)+str+" ");
+		}
+	}
+	
+
+}
---
+++ MyOwn/src/review/ClassReviewEx.java
@@ -0,0 +1,34 @@
+package review;
+
+import java.util.Scanner;
+
+public class ClassReviewEx {
+	
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+//		System.out.print("몇번 출력할까요? ");
+//		ClassReview.printnum( input.nextInt());
+//		
+//		System.out.println();
+//		
+//		System.out.print("실험 ");
+//		System.out.println(input.nextInt());
+//		
+//		System.out.print("번호 입력 ");
+//		int num = input.nextInt();
+//		System.out.print(num);
+//		
+		System.out.print("몇 번 출력할까요? ");
+		int num1 = input.nextInt();
+		input.nextLine();
+		
+		System.out.print("무엇을 출력할까요? ");
+		String str = input.nextLine();
+		
+		ClassReview.printstr(num1, str);
+		
+		
+	}
+
+}
